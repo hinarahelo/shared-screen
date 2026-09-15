6713c81996f3dee1a0399c6142d6ce4f63817bc6
The Mônia <hinarahelo@gmail.com>
2026-09-15 13:23:54 -0400

    Create utils.js

---
+++ server/utils.js
@@ -0,0 +1,25 @@
+const crypto = require('crypto');
+
+function generateRoomId(length = 8) {
+  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+  let result = '';
+  const bytes = crypto.randomBytes(length);
+  for (let i = 0; i < length; i++) {
+    result += chars[bytes[i] % chars.length];
+  }
+  return result;
+}
+
+function sanitizeText(str) {
+  if (typeof str !== 'string') return '';
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+    .trim()
+    .slice(0, 500);
+}
+
+module.exports = { generateRoomId, sanitizeText };
